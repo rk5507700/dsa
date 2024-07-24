362d83eeee6fd573c23001cc2af97388008301f8
Rahul Kumawat <rahulkumawat@rk5507700.local>
2024-07-24 20:30:54 +0530

    v1.1.0

--- Stack/03_stack.c
+++ Stack/03_stack.c
@@ -1,11 +1,21 @@
 #include<stdio.h>
 #include<stdlib.h>
-
+// Node of the stack
 struct node{
     int data;
     struct node* next;
 };
 
+// Method for checking if stack is empty
+int isEmpty(struct node* top){
+    if(top == NULL){
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+// Method for checking if stack is full
 int isFull(){
     struct node* p = (struct node*)malloc(sizeof(struct node));
     if(p == NULL){
@@ -16,6 +26,7 @@ int isFull(){
     }
 }
 
+// Method for pushing an element onto the stack
 struct node* push(struct node* top, int data){
     if(isFull()){
         return top;
@@ -28,10 +39,22 @@ struct node* push(struct node* top, int data){
 
 }
 
+// Method for popping out the element from the stack
+struct node* pop(struct node* top){
+    if(isEmpty(top)){
+        return top;
+    } else {
+        printf("Popped %d from the stack.\n", top->data);
+        top = top->next;
+        return top;
+    }
+}
 int main(){
     struct node* top;
     top = push(top, 56);
     printf("\n %d\n ", top->data);
+    top = pop(top);
+    printf("%d", top->data);
 
     return 0;
 }
