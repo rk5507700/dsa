5330499615847c3c1437b755e660b6b34a69f474
Rahul Kumawat <rahulkumawat@rk5507700.local>
2024-07-25 18:36:00 +0530

    v6.0.0 -functions properly

--- Stack/04_stack.c
+++ Stack/04_stack.c
@@ -1,88 +1,126 @@
 #include<stdio.h>
 #include<stdlib.h>
-
-struct node{
-    int data;
-    struct node* next;
+// Basic definition of stack 
+struct stack{
+    int size;
+    int top;
+    char* arr;
 };
 
-// Empty operation for stack
-int isEmpty(struct node* top){
-    if(top == NULL){
+// Defining the function / method for checking if the list is empty
+int isEmpty(struct stack *s){
+    if (s->top == -1)
+    {
         return 1;
     } else {
-        
         return 0;
     }
+    
 }
 
-int isFull(struct node* top){
-    struct node* p = (struct node*)malloc(sizeof(struct node));
-    if(p == NULL){
+// Defining the function / method for checking if the list is full
+int isFull(struct stack * s){
+    if (s->top == ((s->size) -1))
+    {
         return 1;
     } else {
-        free(p);
         return 0;
     }
+    
 }
 
-// Push method for stack
-struct node* push(struct node* top, int data){
-    struct node* p = (struct node*)malloc(sizeof(struct node));;
-    if(p == NULL){
-        printf("Stack overflow.\n");
-        return top;
-    } else{
-        p->next = top;
-        p->data = data;
-        return p;
-    }
-
-
-}
-
-// Pop method for stack
-
-struct node* pop(struct node* top){
-    struct node* p = NULL;
-    if(isEmpty(top)){
-        printf("Stack underflow.\n");
-        return NULL;
-
+// Defining the function for pushing an element on the stack 
+int push(struct stack* s, char data){
+    if(isFull(s)){
+        printf("Error! Stack Overflow.\n ");
+        return 0;
     } else {
-        p = top;
-        printf("Popped out %d from stack.\n", p->data);
-        top = top->next;
-        free(p);
-        if(top != NULL){
-
-            printf("%d\n", top->data);
-        }
-        return top;
+        s->arr[s->top +1] = data;
+        //printf("Pushed %c successfully.\n", data);
+        s->top += 1;
+        return 1;
     }
 }
 
-
-int parenthesis_checking(char* arr, struct node* top){
-    for(int i = 0; i<50; i++){
-
-        if((*arr)[i] == ')'){
-            if (top == NULL){
-                printf("\n");
-            } else {
-            top = pop(top);
-            }
-        
-        }
-
-        if((*arr)[i] == '('){
-            
-        }
+// Defining the function for poping out an element from the element
+void pop(struct stack* s){
+    int i = s->top;
+    if(isEmpty(s)){
+        printf("Error! Stack underflow.\n");
+    } else {
+        s->top = s->top-1;
+        //printf("Popped %c from the stack.\n", s->arr[s->top+1]);
     }
 }
 
+// Defining the function for traversal of the stack
+int show(struct stack* s){
+    if(isEmpty(s)){
+        printf("Stack Underflow.\n");
+        return 0;
+    } else {
+        int i = 0;
+        while(i <= s->top){
+            printf("%c  ", s->arr[s->top -i]);
+            i++;
+        }
+        printf("\n");
+        return 1;
+    }
+}
+
+// Defining the function/method for peek operation
+/*int peek(struct stack* sp, int i){
+    if(isEmpty(sp) || (sp->top-i < 0)){
+        return -1;
+    } else {
+        return sp->top - i +1;
+    }
+}*/
+
+char stackTop(struct stack* s){
+    if(isEmpty(s)){
+        return -1;
+    } else
+        return s->arr[s->top];
+}
+
+char stackBottom(struct stack * s){
+    if(isEmpty(s)){
+        return -1;
+    } else
+        return s->arr[0];
+}
 int main(){
-    char arr[50];
-    scanf("%s", arr[0]);
+    struct stack* s1;
+    s1 = (struct stack*)malloc(sizeof(struct stack));
+    s1->size = 50;
+    s1->top = -1;
+    s1->arr = (char*)malloc((s1->size) * sizeof(char));
 
+    char demo[50] ;
+    printf("type expression : ");
+    scanf("%s", &demo[0]);
+    int i = 0;
+    int p_count = 0, pp_count = 0;
+    while(demo[i] != '\0'){
+        if(demo[i] == ')'){
+            if(s1->top != -1)
+                pop(s1);
+            pp_count += 1;
+        }
+        if(demo[i] == '('){
+            push(s1, '(');
+            p_count += 1;
+        }
+        i++;
+    }
+    if(s1->top == -1 && pp_count == p_count){
+        printf("parenthesis matching.\n");
+    } else {
+        printf("Parenthesis not matching.\n");
+    }
+
+    
+    return 0;
 }
