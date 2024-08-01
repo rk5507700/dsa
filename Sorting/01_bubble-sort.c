a2f38fa54554431eb2041cacdd69beafbd01a79b
Rahul Kumawat <rahulkumawat@rk5507700.local>
2024-08-01 21:32:23 +0530

    v2.0.0

--- Sorting/01_bubble-sort.c
+++ Sorting/01_bubble-sort.c
@@ -7,6 +7,14 @@ void printArray(int * A, int n){
     printf("\n");
 }
 
+int strlength(int* A){
+    int i = 0;
+    while(A[i] != '\0'){
+        i++;
+    }
+    return i;
+}
+
 void bubbleSort(int* A, int n){
     int i = 0;
     int j = 0;
@@ -21,13 +29,34 @@ void bubbleSort(int* A, int n){
     }
 }
 
+void bubbleSortAdaptive(int* A, int n){
+    int i = 0;
+    int j = 0;
+    int isSorted = 0;
+    for(;i<n;i++){
+        isSorted = 1;
+        printf("Working on pass number %d\n", i+1);
+        for(j = 0;j<n-i-1;j++){
+            if(A[j] > A[j+1]){
+                int temp = A[j];
+                A[j] = A[j+1];
+                A[j+1] = temp;
+                isSorted = 0;
+            }
+        }
+        if(isSorted){
+            return;
+        }
+    }
+}
+
 int main(int argc, char const *argv[])
 {
-    int A[] = {23, 20, 12, 9, 7, 6, 5, 4};
+    int A[] = {3, 2, 1};
     printf("The array before sorting: ");
-    printArray(A, 6);
-    bubbleSort(A, 6);
+    printArray(A, 3);
+    bubbleSortAdaptive(A, 3);
     printf("The array after sorting: ");
-    printArray(A, 6);
+    printArray(A, 3);
     return 0;
 }
