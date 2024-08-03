7719a37556b37076d7c91af4b3a1e7f5f100ba99
Rahul Kumawat <rahulkumawat@rk5507700.local>
2024-08-03 22:00:34 +0530

    v1.0.0

---
+++ Sorting/03_Selection_sort.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int* selectionSort(int* a, int size_of_array){
+
+}
+
+// int arraySize(int* arr){
+//     int i = 0;
+//     while(arr[i] != 0){
+//         i++;
+//     }
+//     return i;
+// }
+
+int main(int argc, char const *argv[])
+{   
+    int arr[] = {30, 70, 58, 64, 34};
+    
+    return 0;
+}
+
+
