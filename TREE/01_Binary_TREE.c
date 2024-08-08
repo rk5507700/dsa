f75bcf8ce2b35244928e5095c01c493f80c696c5
Rahul Kumawat <rahulkumawat@rk5507700.local>
2024-08-08 22:08:34 +0530

    v2.0.0

--- TREE/01_Binary_TREE.c
+++ TREE/01_Binary_TREE.c
@@ -1,3 +1,14 @@
+/*
+          4
+         / \
+        1   2
+       / \   \
+      5   7   34
+     / \
+    99 78
+  
+*/
+
 #include<stdio.h>
 #include<stdlib.h>
 struct binary_node
@@ -18,9 +29,36 @@ struct binary_node* createNode(int data, struct binary_node* parent){
     return p1;
 }
 
+void preOrder(struct binary_node* root){
+    if(root != NULL){
+        printf("%d ", root->data);
+        preOrder(root->left);
+        preOrder(root->right);
+    }
+}
+
 int main(){
-    struct binary_node* root = createNode(11, NULL);
-    struct binary_node* p1_right = createNode(10, root);
-    struct binary_node* p1_left = createNode(8, root);
+    struct binary_node* root = createNode(4, NULL);
+    struct binary_node* p1_right = createNode(2, root);
+    struct binary_node* p1_left = createNode(1, root);
+    struct binary_node* p2_right = createNode(7, p1_left);
+    struct binary_node* p2_left = createNode(5, p1_left);
+    struct binary_node* p3_left = createNode(99, p2_left);
+    struct binary_node* p3_right = createNode(78, p2_left);
+    struct binary_node* p1_right_right_child = createNode(34, p1_right);
+    // struct binary_node* p3_right = createNode(78, p2_left);
+    
+
+    root->right = p1_right;
+    root->left = p1_left;
+
+    p1_left->right = p2_right;
+    p1_left->left = p2_left;
+
+    p2_left->left = p3_left;
+    p2_left->right = p3_right;
+
+    p1_right->right = p1_right_right_child;
+    preOrder(root);
     return 0;
 }
