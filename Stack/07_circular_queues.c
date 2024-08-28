bd3ef0e2f312103b0682478cd4e5a15ea7d646a7
rk5507700 <rk5507700@gmail.com>
2024-08-28 16:13:54 +0530

    Newly written version with 1. Enqueue, 2.Dequeue, and 3.isEmpty methods

--- Stack/07_circular_queues.c
+++ Stack/07_circular_queues.c
@@ -1,77 +1,87 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-struct queue
-{
+struct node{
     int size;
     int front;
     int rear;
     int* arr;
 };
 
-void enqueue(struct queue* q){
-    if((q->rear+1)%q->size == q->front)
+int isEmpty(struct node* q){
+    if(q->front == -1 || q->arr[q->front] == '\0')
+        return 1;
+    else 
+        return 0;
+}
+
+void enqueue(struct node* q, int data){
+    if((q->rear + 1)%q->size == q->front && q->arr[q->front] != '\0'){
         printf("Queue is full.\n");
+    } else {
+        if(q->rear == -1){
+            q->rear = 0;
+            q->arr[q->rear] = data;
+            }
+        else 
+        {
+            q->rear = (q->rear+1)%q->size;
+            q->arr[q->rear] = data;
+        }
+        if(q->front == -1)
+            q->front = 0;
+    }
+}
+
+int dequeue(struct node* q){
+    if(isEmpty(q))
+        return -1;
     else {
-        q->rear = (q->rear+1)%q->size;
-        printf("Enter the element to queue: ");
-        scanf("%d", &(q->arr[q->rear]));
-        if(q->arr[q->front] == '\0')
-            q->front =(q->front+1)%q->size;
+        int i = q->arr[q->front];
+        q->arr[q->front] = '\0';
+        q->front = (q->front + 1)%q->size;
+        return i;
     }
 }
 
-void dequeue(struct queue* q){
-    if(q->arr[q->front] == '\0' || q->front == -1){
-        printf("Queue is empty.\n");
-    } else {
-        printf("Element %d dequeued.\n", q->arr[q->front]);
-        if(q->front == q->rear)
-            q->arr[q->front] = '\0';
-        else
-            q->front =(q->front+1)%q->size;
-    }
-}
-
-void display(struct queue* q){
-    if(q->arr[q->front] == '\0' || q->front == -1){
-        printf("Queue is empty.\n");
-    } else {
-        printf("Element %d is queued at the front.\n", q->arr[q->front]);
-    }
-}
-
-int main(){
-    struct queue* q;
-    q = (struct queue*)malloc(sizeof(struct queue));
-    q->front = -1;
+int main(int argc, char const *argv[])
+{
+    struct node* q;
+    q = (struct node*)malloc(sizeof(struct node));
+    q->size = 7;
     q->rear = -1;
-    q->size = 5;
-    q->arr = (int*)malloc(q->size*sizeof(int));
-
-    printf("1.Enqueue.\n");
-    printf("2.Dequeue.\n");
-    printf("3.Display.\n");
-    printf("4.Exit.\n");
-
-    while(1){
-        int choice;
-        printf("Enter your choice : ");
+    q->front = -1;
+    q->arr = (int*)malloc((q->size)*sizeof(int));
+    printf("1.Enqueue\n");
+    printf("2.Dequeue\n");
+    printf("3.Exit\n");
+    while (1)
+    {
+        int choice, data;
         scanf("%d", &choice);
-
-        switch(choice){
-            case 1:enqueue(q); 
-                break;
-            case 2: dequeue(q); 
-                break;
-            case 3: display(q); 
-                break;
-            case 4:exit(0); 
-                break;
-            default : printf("Invalid input.\n"); 
-                break;
+        switch (choice)
+        {
+        case 1:{ 
+            printf("Enter the data: ");
+            scanf("%d", &data);
+            enqueue(q, data);}
+            break;
+        case 2:
+            {
+            data = dequeue(q);
+            if(data == -1){
+                printf("Queue is empty.\n");
+            }
+            else {
+            printf("Dequeued %d\n", data);
+            }
+            }
+            break;
+        case 3: exit(0);
+        default:
+            break;
         }
     }
-
+    
     return 0;
 }
