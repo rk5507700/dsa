af0edce0208675d12e8c5bec4616a2547275e95c
rk5507700 <rk5507700@gmail.com>
2024-08-29 17:12:34 +0530

    Sorted the files

--- Stack/05_Infix->postfix.c
+++ Stack/05_Infix->postfix.c
@@ -1,32 +1,17 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-struct stack{
+struct stack
+{
     int size;
     int top;
-    char* arr;
+    char *arr;
 };
 
-void push(struct stack* stack, char data){
-    if(stack->top == (stack->size-1)){
-        printf("stack overflow.\n");
-    } else {
-        stack->top += 1;
-        stack->arr[stack->top] = data;
-    }
-}
-
-void pop(struct stack* stack){
-    if(stack->top == -1){
-        printf("Stack underflow.\n");
-    } else {
-        printf("Popped %d from the stack.\n", stack->arr[stack->top]);
-        stack->top -= 1;
-    }
-}
-
-int isOperator(char s){
-    switch (s)
+int isOperator(char c)
+{
+    switch (c)
     {
     case '*':
         return 1;
@@ -34,10 +19,10 @@ int isOperator(char s){
     case '/':
         return 1;
         break;
-    case '+':
+    case '-':
         return 1;
         break;
-    case '-':
+    case '+':
         return 1;
         break;
     default:
@@ -46,70 +31,98 @@ int isOperator(char s){
     }
 }
 
-int precedence(char c){
+void push(struct stack *s, char c)
+{
+    s->arr[s->top + 1] = c;
+    s->top += 1;
+}
+
+char pop(struct stack *s)
+{
+    if (s->top == -1)
+    {
+        printf("Stack underflow.\n");
+        return '\0';
+    }
+    else
+    {
+        char c = s->arr[s->top];
+        s->top -= 1;
+        return c;
+    }
+}
+
+int precedence(char c)
+{
     switch (c)
     {
-    case '*': return 2;
-        /* code */
+    case '*':
+        return 2;
         break;
-
-    case '/': return 2;
-        /* code */
+    case '/':
+        return 2;
         break;
-    
-    default: return 1;
+    case '-':
+        return 1;
+        break;
+    case '+':
+        return 1;
+        break;
+    default:
+        return 0;
         break;
     }
 }
 
-int main(){
-    struct stack* stack;
-    stack = (struct stack*)malloc(sizeof(struct stack));
-    stack->size = 20;
+char stackTop(struct stack *s)
+{
+    if (s->top == -1)
+        return '\0';
+    else
+        return s->arr[s->top];
+}
+
+char *inToPos(char *infix)
+{
+    struct stack *stack = (struct stack *)malloc(sizeof(struct stack));
     stack->top = -1;
-    stack->arr = (char*) malloc((stack->size)*sizeof(char));
-
-    char usr_arr[20];
-    char new_arr[20];
-
-    printf("Enter your expression: ");
-    scanf("%s\n", &usr_arr[0]);
+    stack->size = strlen(infix);
+    stack->arr = (char *)malloc(stack->size * sizeof(char));
+    char *postfix_arr = (char *)malloc((stack->size + 1) * sizeof(char));
 
     int i = 0;
     int j = 0;
 
-    while(usr_arr[i] != '\0'){
-        if(!isOperator(usr_arr[i])){
-            new_arr[j] = usr_arr[i];
+    while (infix[i] != '\0')
+    {
+        if (!isOperator(infix[i]))
+        {
+            postfix_arr[j] = infix[i];
             j++;
-        } else {
-            if(stack->top == -1){
-                push(stack, usr_arr[i]);
-            } else {
-                if(precedence(usr_arr[i])<= precedence(stack->arr[stack->top])){
-                    while(precedence(usr_arr[i])<= precedence(stack->arr[stack->top])){
-                        new_arr[j] = stack->arr[stack->top];
-                        stack->top -= 1;
-                        j++;
-                        if(stack->top == -1)
-                            break;
-                    }
-                } else {
-                    push(stack, usr_arr[i]);
-                }
-
+        }
+        else
+        {
+            while(!(precedence(infix[i]) > precedence(stackTop(stack))))
+            {
+                postfix_arr[j] = pop(stack);
+                j++;
             }
+            push(stack, infix[i]);
+            
         }
         i++;
     }
     while(stack->top != -1){
-        new_arr[j] = stack->arr[stack->top];
-        j++;
-        stack->top -= 1;
+        postfix_arr[j] = pop(stack);
     }
-    printf("%s", new_arr);
-
+    return postfix_arr;
+}
+int main()
+{
+    char *arr = (char *)malloc(30 * sizeof(char));
+    printf("Enter your expression: ");
+    scanf("%s", &arr[0]);
+    char* pos = inToPos(arr);
+    printf("%s", pos);
     return 0;
-
-
 }
